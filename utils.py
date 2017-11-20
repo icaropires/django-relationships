157cfc166ed8fd61b9067f4c7e4245acf0fb22d0
Icaro Pires <icaropsa@gmail.com>
2017-11-20 20:15:07 -0200

    Previous project things

---
+++ utils.py
@@ -0,0 +1,19 @@
+from rest_framework_jwt.settings import api_settings
+from rest_framework.test import APIClient
+
+
+def get_token(user):
+    payload = api_settings.JWT_PAYLOAD_HANDLER(user)
+    token = api_settings.JWT_ENCODE_HANDLER(payload)
+    return token
+
+
+def authenticate_user(user):
+    django_client = APIClient()
+
+    user_token = get_token(user)
+    token = 'JWT {}'.format(user_token)
+
+    django_client.credentials(HTTP_AUTHORIZATION=token)
+
+    return django_client
