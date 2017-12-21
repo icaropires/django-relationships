a4d8bbdddb8daa84bb9cdb2b037225b173a4002f
Ícaro Pires de Souza Aragão <icaropsa@gmail.com>
2017-12-21 15:04:34 -0200

    Add nested relationship support for Relationship
    Signed-off-by: Ícaro Pires de Souza Aragão <icaropsa@gmail.com>

--- utils.py
+++ utils.py
@@ -1,19 +1,66 @@
-from rest_framework_jwt.settings import api_settings
-from rest_framework.test import APIClient
+class Relationship:
+    def __init__(self, primary, secondary, many=None, relationship_attr=None):
+        self.many, self.relationship_attr = many, relationship_attr
+        self.primary, self.secondary = primary, secondary
 
+        if many is not None and relationship_attr:  # If has enough info
+            self.make()
 
-def get_token(user):
-    payload = api_settings.JWT_PAYLOAD_HANDLER(user)
-    token = api_settings.JWT_ENCODE_HANDLER(payload)
-    return token
+    def make(self, primary=None, secondary=None,
+             many=None, relationship_attr=None):
 
+        self._fill_missing_attributes(primary, secondary, many,
+                                      relationship_attr)
 
-def authenticate_user(user):
-    django_client = APIClient()
+        if not self._has_relationship():
+            raise AttributeError('There is no relationship between'
+                                 ' user and consulted classes')
 
-    user_token = get_token(user)
-    token = 'JWT {}'.format(user_token)
+        # Avoid typing :)
+        primary, secondary, many, attr = self.primary, self.secondary, \
+            self.many, self.relationship_attr
 
-    django_client.credentials(HTTP_AUTHORIZATION=token)
+        if many is None and not attr:
+            raise Exception('Not enough information for making relationship')
 
-    return django_client
+        if many:
+            attr = getattr(secondary, attr)
+            attr.add(primary)  # Making relationship 1 or n to many
+        else:
+            secondary.delete()  # Avoid unique constraint problems
+            setattr(secondary, attr, primary)  # Making relationship 1 to 1
+
+            try:
+                secondary.save()
+            except ValueError:
+                primary_name = primary.__class__.__name__
+                secondary_name = secondary.__class__.__name__
+
+                raise ValueError("Wasn't possible to create relationship"
+                                 " between {} and {} models"
+                                 .format(primary_name, secondary_name))
+
+    def get_info(self):
+        return (self.many, self.relationship_attr,
+                self.primary, self.secondary)
+
+    def _has_relationship(self):
+
+        # Primary will have primary key and secondary the foreign key
+        # hasattr called twice to test A to B and B to A relationships
+        if not hasattr(self.secondary, self.relationship_attr):
+            self.primary, self.secondary = self.secondary, self.primary
+
+        return hasattr(self.secondary, self.relationship_attr)
+
+    # Can't use None because None will be used to check what params were passed
+    def _fill_missing_attributes(self, primary=None, secondary=None,
+                                 many=None, relationship_attr=None):
+
+        self.primary = primary or self.primary
+        self.secondary = secondary or self.secondary
+        self.many = many or self.many
+        self.relationship_attr = relationship_attr or self.relationship_attr
+
+    def __str__(self):
+        return str(self.get_info())
